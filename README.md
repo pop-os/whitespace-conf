ecba5107ed7dbfa20c4fa8bf4275287c619ac238
Michael Aaron Murphy <mmstick@pm.me>
2021-06-27 18:11:38 -0600

    fix: Crate name invalid in example

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ use std::fs;
 fn main() {
     let string = fs::read_to_string("/etc/login.defs").unwrap();
 
-    let defs = linux_login_defs::parse(&string);
+    let defs = whitespace_conf::parse(&string);
 
     println!("UID_MIN = {:?}", defs.get("UID_MIN"));
     println!("UID_MAX = {:?}", defs.get("UID_MAX"));
--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //! fn main() {
 //!     let string = fs::read_to_string("/etc/login.defs").unwrap();
 //!
-//!     let defs = linux_login_defs::parse(&string);
+//!     let defs = whitespace_conf::parse(&string);
 //!
 //!     println!("UID_MIN = {:?}", defs.get("UID_MIN"));
 //!     println!("UID_MAX = {:?}", defs.get("UID_MAX"));
